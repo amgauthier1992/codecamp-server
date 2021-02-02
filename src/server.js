489cebe5917ffec714d7625c01f150e700c80954
Andy Gauthier <amgauthier1992@gmail.com>
2021-02-02 13:11:25 -0500

    updated for deploy

---
+++ src/server.js
@@ -0,0 +1,18 @@
+const knex = require('knex');
+const app = require('./app');
+const { PORT, DATABASE_URL } = require('./config');
+// const { PORT, TEST_DATABASE_URL } = require('./config');
+
+pg.defaults.ssl = process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false;
+
+const db = knex({
+  client: 'pg',
+  connection: DATABASE_URL,
+  // connection: TEST_DATABASE_URL,
+});
+
+app.set('db', db);
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
